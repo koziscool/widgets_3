936f03d5054523441d754ee8605bd4bc31b31e70
koziscool <kozjob@yahoo.com>
2016-07-10 17:53:32 -0500

    form works table works

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ var widgets = angular.module('widgets', []);
 widgets.controller( 'RestaurantCtrl', 
   ['$scope', function($scope){
 
-    
-    
+    $scope.restaurants = [];
+
+    $scope.processForm = function() {
+      console.log("here");
+      var newRestaurant = {};
+
+      newRestaurant.name = $scope.formData.name;
+      newRestaurant.foodType = $scope.formData.type;
+
+      $scope.restaurants.push( newRestaurant );
+    };
+
   }]);
 
